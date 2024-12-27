902afe88f0e6f2ebba870e97949174ac60c8bc7f
Fuxin Zhang <fxzhang@ict.ac.cn>
2024-12-27 14:45:58 +0800

    various setting adjustments: 1. API level 2. full_treble 3. add hwservicemanager and hidl_allocator to product_packages ...

--- loongson_3a5000/device_la_3a5000.mk
+++ loongson_3a5000/device_la_3a5000.mk
@@ -1,6 +1,5 @@
-# FIXME API_LEVEL 32 required manifest fcm verison ge 6
-# but now is 3
-#PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 35
+PRODUCT_FULL_TREBLE_OVERRIDE := true
 
 DEVICE_MANIFEST_FILE += \
     device/loongson/loongsonboard/manifest.xml
@@ -16,6 +15,11 @@ PRODUCT_BUILD_VENDOR_IMAGE := true
 PRODUCT_BUILD_USERDATA_IMAGE := true
 PRODUCT_BUILD_SUPER_PARTITION := true
 PRODUCT_BUILD_CACHE_IMAGE := true
+#PRODUCT_BUILD_VBMETA_IMAGE := true
+
+PRODUCT_PACKAGES += \
+    hwservicemanager \
+    android.hidl.allocator@1.0-service
 
 ############################# All HALs  ##############################
 # system configurations
@@ -25,10 +29,10 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/init.loongson_3a5000.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.loongson_3a5000.rc \
     $(LOCAL_PATH)/../common/Generic.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Generic.kl \
     $(LOCAL_PATH)/../common/displayconfig/default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/default.xml \
+    $(LOCAL_PATH)/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
+	$(LOCAL_PATH)/la_net.rc:$(TARGET_COPY_OUT_VENDOR)/etc/la_net.rc \
+	$(LOCAL_PATH)/la_net.sh:$(TARGET_COPY_OUT_VENDOR)/bin/la_net.sh \
     $(PRODUCT_COPY_FILES)
 
 
-    #$(LOCAL_PATH)/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
-	#$(LOCAL_PATH)/la_net.rc:$(TARGET_COPY_OUT_VENDOR)/etc/la_net.rc \
-	#$(LOCAL_PATH)/la_net.sh:$(TARGET_COPY_OUT_VENDOR)/bin/la_net.sh \
 
